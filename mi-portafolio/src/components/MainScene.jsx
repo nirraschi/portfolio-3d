13a6800bc8d46728bba05c95738bac4bc66b0cbb
nirraschi <nirraschi@outlook.com>
2026-04-15 22:19:46 -0300

    isla con room

--- mi-portafolio/src/components/MainScene.jsx
+++ mi-portafolio/src/components/MainScene.jsx
@@ -3,15 +3,22 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from "@react-three/drei";
 import Obj from './Obj'
 import Island from './Island';
+import {Environment} from '@react-three/drei';
+import Room from './Room';
+
 
 const MainScene = () => {
     return (
-        <section className='h-screen w-screen overflow-x-hidden bg-amber-100'>
+        <section className='h-screen w-screen overflow-x-hidden bg-[#dedad1]'>
 
-            <Canvas camera={{ position: [0, 2, 20], fov: 50 }}>
-                <ambientLight intensity={7} />
+            <Canvas camera={{ position: [0, 4, 80], fov: 50 }}>
+                <ambientLight intensity={3} />
+                <directionalLight position={[0, 2, 10]} intensity={3} />
                 <OrbitControls />
-                <Island path='/obj/island.glb'  scale={0.1} position={[0, 0, 0]} emissive={"#e0d9c5"} emissiveIntensity={1} />
+                <Island path='/obj/island.glb'  scale={0.5} position={[0, 0, 0]} />
+                <Environment preset='city' />
+                <Room path='/obj/room.glb' scale={5} position={[0, 2, 0]} />
+
             </Canvas>
 
 
